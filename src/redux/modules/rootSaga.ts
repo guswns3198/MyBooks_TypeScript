3d1899499197162968b353e66ad89f666035f0ce
guswns3198 <guswns3198@naver.com>
2023-03-14 23:55:42 +0900

    rootSaga

--- src/redux/modules/rootSaga.ts
+++ src/redux/modules/rootSaga.ts
@@ -2,5 +2,7 @@ import { all } from "redux-saga/effects";
 import { authSaga } from "./auth";
 
 export default function* rootSaga() {
-  yield all([authSaga()]);
+  yield all(
+    [authSaga()]
+  );
 }
